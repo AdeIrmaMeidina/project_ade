fc9c1b25c9492b8210a6c74942107e903d3633a0
AdeIrmaMeidina <airma9110@gmail.com>
2024-04-25 09:00:27 +0700

    Ini Project yang di Push Pertama Oleh Ade Irma rpl4

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Selamat datang di halaman biodata</h1>
+    <p>Nama : {{$nama}}</p>
+    <p>Jenis kelamin : {{$jenis_kelamin}}</p>
+    <p>Alamat : {{$alamat}}</p>
+    <p>Pendidikan : {{$pendidikan}}</p>
+    <p>Pekerjaan : {{$pekerjaan}}</p>
+</body>
+</html>
